445a08286ca4dea67b17679d9b53cc3d651d5ad8
shervin.emami <shervin.emami@seeingmachines.com>
2018-12-31 16:36:40 +1100

    _aenea.py:51: [31m-try:[m _aenea.py:51: [31m-    import pkg_resources[m _aenea.py:51: [31m-    dragonfly_dist = pkg_resources.get_distribution("dragonfly")[m _aenea.py:51: [31m-    print "Current Dragonfly version:", dragonfly_dist.version[m _aenea.py:51: [31m-except ImportError:[m _aenea.py:51: [31m-    pass[m _aenea.py:51: [31m-[m

--- _aenea.py
+++ _aenea.py
@@ -50,13 +50,6 @@ print '\tUSE_MULTIPLE_ACTIONS:', aenea.config.USE_MULTIPLE_ACTIONS
 print '\tSCREEN_RESOLUTION:', aenea.config.SCREEN_RESOLUTION
 
 try:
-    import pkg_resources
-    dragonfly_dist = pkg_resources.get_distribution("dragonfly")
-    print "Current Dragonfly version:", dragonfly_dist.version
-except ImportError:
-    pass
-
-try:
     aenea.proxy_contexts._get_context()
     print 'Aenea: Successfully connected to server.'
 except:
