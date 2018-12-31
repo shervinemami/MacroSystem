d62ffda3b70d44cf8b3b5c90ec4a14f3909b991d
shervin.emami <shervin.emami@seeingmachines.com>
2019-01-01 01:11:11 +1100

    keyboard.py:135: [31m-p[m keyboard.py:135: [31m-mnpmn[m keyboard.py:135: [32m+[m keyboard.py:136: [32m+[m

--- keyboard.py
+++ keyboard.py
@@ -132,8 +132,8 @@ singleModifierMap = {
     "shift": "shift",
     "super": "win",
 }
-p
-mnpmn
+
+
 letterMap = {
     "(as) ": "a",                                   #"(aim|and) ": "a",		# careful of 8, @, lace, lack. My "aim" sometimes gets picked up as "and".
     "(black) ": "b",        #  "best" sometimes gets picked up as "this" or "guess". "B|the" sometimes gets picked up as "enter"
