1b10e913e0e82ca38c987abce13a46a7445864c3
shervin.emami <shervin.emami@seeingmachines.com>
2018-12-31 23:35:54 +1100

    programs.py:102: [31m-        "caps lock":     Key("ctrl:down/3, win:down/3, alt:down/3, z") + Key("ctrl:up, win:up, alt:up"),[m programs.py:102: [32m+[m[32m        "press caps lock":     Key("ctrl:down/3, win:down/3, alt:down/3, z") + Key("ctrl:up, win:up, alt:up"),[m

--- programs.py
+++ programs.py
@@ -99,7 +99,7 @@ class ProgramsRule(MappingRule):
         "close tab":     Key("ctrl:down/3, win:down/3, alt:down/3, shift:down/3, c") + Key("ctrl:up, win:up, alt:up, shift:up"),
 
         # My current aenea proxy system isn't allowing to hold down shift or caps lock, so try using Linux AutoKey instead
-        "caps lock":     Key("ctrl:down/3, win:down/3, alt:down/3, z") + Key("ctrl:up, win:up, alt:up"),
+        "press caps lock":     Key("ctrl:down/3, win:down/3, alt:down/3, z") + Key("ctrl:up, win:up, alt:up"),
 
         # Konsole shortcuts
         "search": Key("c-r"),   # Ctrcl+R
