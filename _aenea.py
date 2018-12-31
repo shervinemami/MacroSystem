0d8c1c0c445c5ad5ef81f08a4678a41ffffea008
shervin.emami <shervin.emami@seeingmachines.com>
2018-12-31 16:27:21 +1100

    _aenea.py:53: [31m-    pkg_resources.require("dragonfly >= 0.6.5beta1.dev-r81")[m

--- _aenea.py
+++ _aenea.py
@@ -51,7 +51,6 @@ print '\tSCREEN_RESOLUTION:', aenea.config.SCREEN_RESOLUTION
 
 try:
     import pkg_resources
-    pkg_resources.require("dragonfly >= 0.6.5beta1.dev-r81")
     dragonfly_dist = pkg_resources.get_distribution("dragonfly")
     print "Current Dragonfly version:", dragonfly_dist.version
 except ImportError:
