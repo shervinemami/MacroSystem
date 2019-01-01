c1a2831918033d2bacb0fe88edf7d522c261cdf0
shervin.emami <shervin.emami@seeingmachines.com>
2019-01-01 13:05:39 +1100

    _all.py:15: [31m-alternatives.append(RuleRef(rule=words.ReFormatRule()))[m _all.py:15: [31m-alternatives.append(RuleRef(rule=words.NopeFormatRule()))[m _all.py:15: [32m+[m[32m#alternatives.append(RuleRef(rule=words.ReFormatRule()))[m _all.py:16: [32m+[m[32m#alternatives.append(RuleRef(rule=words.NopeFormatRule()))[m

--- _all.py
+++ _all.py
@@ -12,8 +12,8 @@ release = Key("shift:up, ctrl:up, alt:up")
 alternatives = []
 alternatives.append(RuleRef(rule=keyboard.KeystrokeRule()))
 alternatives.append(RuleRef(rule=words.FormatRule()))
-alternatives.append(RuleRef(rule=words.ReFormatRule()))
-alternatives.append(RuleRef(rule=words.NopeFormatRule()))
+#alternatives.append(RuleRef(rule=words.ReFormatRule()))
+#alternatives.append(RuleRef(rule=words.NopeFormatRule()))
 alternatives.append(RuleRef(rule=programs.ProgramsRule()))
 root_action = Alternative(alternatives)
 
