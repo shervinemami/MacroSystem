8c5ecc2dbbf53c96ca8767c7e4ff592eb9ed31c7
shervin.emami <shervin.emami@seeingmachines.com>
2019-01-03 18:37:26 +1100

    keyboard.py:171: [31m-    upperLetterMap["big " + letter] = letterMap[letter].upper()         #[m keyboard.py:171: [32m+[m[32m    upperLetterMap["bam " + letter] = letterMap[letter].upper()         #[m keyboard.py:172: [32m+[m[32m    #upperLetterMap["big " + letter] = letterMap[letter].upper()         # My "big" is pretty good, but usually fails "big yeelax"[m

--- keyboard.py
+++ keyboard.py
@@ -168,7 +168,8 @@ letterMap = {
 # generate uppercase versions of every letter
 upperLetterMap = {}
 for letter in letterMap:
-    upperLetterMap["big " + letter] = letterMap[letter].upper()         #
+    upperLetterMap["bam " + letter] = letterMap[letter].upper()         #
+    #upperLetterMap["big " + letter] = letterMap[letter].upper()         # My "big" is pretty good, but usually fails "big yeelax"
     #upperLetterMap["case " + letter] = letterMap[letter].upper()         # My "case" is too much like "plus"
     #upperLetterMap["capital " + letter] = letterMap[letter].upper()     # My "cap" is too much like "up"
     #upperLetterMap["sky " + letter] = letterMap[letter].upper()         # My "sky" is too much like "score" :-(
