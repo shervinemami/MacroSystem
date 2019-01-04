48ea7b940520ebb04ab61ceab59cc21ad62f31e7
shervin.emami <shervin.emami@seeingmachines.com>
2019-01-04 20:23:13 +1100

    keyboard.py:305: [31m-        "release all": Key("shift:up, ctrl:up, alt:up, win:up"),[m keyboard.py:305: [32m+[m[32m        "release all": Key("shift:up, ctrl:up, alt:up, win:up,  shift:down, ctrl:down, alt:down, win:down,  shift:up, ctrl:up, alt:up, w

--- keyboard.py
+++ keyboard.py
@@ -302,7 +302,7 @@ grammarCfg.cmd.map = Item(
         "control": Key("ctrl:down/3"),
         #"hold control": Key("ctrl:down"),
         #"release control": Key("ctrl:up"),
-        "release all": Key("shift:up, ctrl:up, alt:up, win:up"),
+        "release all": Key("shift:up, ctrl:up, alt:up, win:up,  shift:down, ctrl:down, alt:down, win:down,  shift:up, ctrl:up, alt:up, win:up"),
         #"press key <pressKey>": Key("%(pressKey)s"),
 
         # Closures.
