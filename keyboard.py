557f2ef8650ca926c2b3040be3eb7a9c7de2409c
shervin.emami <shervin.emami@seeingmachines.com>
2019-01-04 19:36:39 +1100

    keyboard.py:272: [31m-        "window space": Key("win:down/1") + Key("space")  + Key("win:up"),[m keyboard.py:272: [31m-        "window up":    Key("win:down/1") + Key("up")     + Key("win:up"),[m keyboard.py:272: [31m-        "window down":  Key("win:down/1") + Key("down")   + Key("win:up"),[m keyboard.py:272: [31m-        "window left":  Key("win:down/1") + Key("left")   + Key("win:up"),[m keyboard.py:272: [31m-        "window right": Key("win:down/1") + Key("right")  + Key("win:up"),[m keyboard.py:272: [31m-        "window enter": Key("win:down/1") + Key("enter")  + Key("win:up"),[m keyboard.py:272: [31m-        "window tab":   Key("win:down/1") + Key("tab")    + Key("win:up"),[m keyboard.py:272: [31m-        "window insert":Key("win:down/1") + Key("insert") + Key("win:up"),[m keyboard.py:272: [32m+[m[32m        "window space": Key("win:down") + Key("space")  + Key("win:up"),[m keyboard.py:273: [32m+[m[32m        "window up":    Key("win:down") + Key("up")     + Key("win:up"),[m keyboard.py:274: [32m+[m[32m        "window down":  Key("win:down") + Key("down")   + Key("win:up"),[m keyboard.py:275: [32m+[m[32m        "window left":  Key("win:down") + Key("left")   + Key("win:up"),[m keyboard.py:276: [32m+[m[32m        "window right": Key("win:down") + Key("right")  + Key("win:up"),[m keyboard.py:277: [32m+[m[32m        "window enter": Key("win:down") + Key("enter")  + Key("win:up"),[m keyboard.py:278: [32m+[m[32m        "window tab":   Key("win:down") + Key("tab")    + Key("win:up"),[m keyboard.py:279: [32m+[m[32m        "window insert":Key("win:down") + Key("insert") + Key("win:up"),[m

--- keyboard.py
+++ keyboard.py
@@ -269,14 +269,14 @@ grammarCfg.cmd.map = Item(
         "window 3": Key("win:down/3") + Text("3") + Key("win:up"),    # Allow to say "window 2" to switch to the 2nd window
         "window 4": Key("win:down/3") + Text("4") + Key("win:up"),    # Allow to say "window 2" to switch to the 2nd window
         "window 5": Key("win:down/3") + Text("5") + Key("win:up"),    # Allow to say "window 2" to switch to the 2nd window
-        "window space": Key("win:down/1") + Key("space")  + Key("win:up"),
-        "window up":    Key("win:down/1") + Key("up")     + Key("win:up"),
-        "window down":  Key("win:down/1") + Key("down")   + Key("win:up"),
-        "window left":  Key("win:down/1") + Key("left")   + Key("win:up"),
-        "window right": Key("win:down/1") + Key("right")  + Key("win:up"),
-        "window enter": Key("win:down/1") + Key("enter")  + Key("win:up"),
-        "window tab":   Key("win:down/1") + Key("tab")    + Key("win:up"),
-        "window insert":Key("win:down/1") + Key("insert") + Key("win:up"),
+        "window space": Key("win:down") + Key("space")  + Key("win:up"),
+        "window up":    Key("win:down") + Key("up")     + Key("win:up"),
+        "window down":  Key("win:down") + Key("down")   + Key("win:up"),
+        "window left":  Key("win:down") + Key("left")   + Key("win:up"),
+        "window right": Key("win:down") + Key("right")  + Key("win:up"),
+        "window enter": Key("win:down") + Key("enter")  + Key("win:up"),
+        "window tab":   Key("win:down") + Key("tab")    + Key("win:up"),
+        "window insert":Key("win:down") + Key("insert") + Key("win:up"),
         "meta [<num>]": Key("alt:down/1") + Text("%(num)d") + release,      # Allow to say "meta 2" to hit Alt+2 to switch to the 2nd tab of Firefox, etc
 		#meta": Key("alt:down/3"),    # Or do I prefer "alter"?
 		"meta 1": Key("alt:down/3") + Text("1") + release,      # Allow to say "meta 2" to hit Alt+2 to switch to the 2nd tab of Firefox, etc
