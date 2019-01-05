9e34f925e073f1c9a98b83b28d0015fe028c6009
shervin.emami <shervin.emami@seeingmachines.com>
2019-01-05 19:30:35 +1100

    programs.py:104: [31m-        # Allow switching to the left menu area of the KDE file input dialogs, even when using applications that aren't Dolphin.[m programs.py:104: [31m-        "dolphin change to left tab":   Key("tab, tab, tab"),[m programs.py:104: [31m-[m programs.py:104: [31m-[m

--- programs.py
+++ programs.py
@@ -102,10 +102,6 @@ class ProgramsRule(MappingRule):
         #"change to right <n> times":    Key("ctrl:down/3, win:down/3, alt:down/3, shift:down/3, r") + Key("ctrl:up, win:up, alt:up, shift:up"),
         "close tab":                    Key("ctrl:down/3, win:down/3, alt:down/3, shift:down/3, c") + Key("ctrl:up, win:up, alt:up, shift:up"),
 
-        # Allow switching to the left menu area of the KDE file input dialogs, even when using applications that aren't Dolphin.
-        "dolphin change to left tab":   Key("tab, tab, tab"),
-
-
         # My current aenea proxy system isn't allowing to hold down shift or caps lock, so try using Linux AutoKey instead
         "press caps lock": Key("ctrl:down/3, win:down/3, alt:down/3, z") + Key("ctrl:up, win:up, alt:up"),
 
