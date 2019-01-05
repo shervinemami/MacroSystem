29d94d715a020363755b5990a91d1fb62a71d58a
shervin.emami <shervin.emami@seeingmachines.com>
2019-01-05 16:35:41 +1100

    programs.py:106: [31m-        "dolphin change to left tab":   Key("ctrl:down/3, win:down/3, alt:down/3, shift:down/3, l") + Key("ctrl:up, win:up, alt:up, shift:up"), programs.py:106: [32m+[m[32m        "dolphin change to left tab":   Key("tab, tab, tab")[m programs.py:107: [32m+[m programs.py:110: [31m-        "press caps lock": Key("tab, tab, tab")[m programs.py:110: [32m+[m[32m        "press caps lock": Key("ctrl:down/3, win:down/3, alt:down/3, z") + Key("ctrl:up, win:up, alt:up"),[m

--- programs.py
+++ programs.py
@@ -103,10 +103,11 @@ class ProgramsRule(MappingRule):
         "close tab":                    Key("ctrl:down/3, win:down/3, alt:down/3, shift:down/3, c") + Key("ctrl:up, win:up, alt:up, shift:up"),
 
         # Allow switching to the left menu area of the KDE file input dialogs, even when using applications that aren't Dolphin.
-        "dolphin change to left tab":   Key("ctrl:down/3, win:down/3, alt:down/3, shift:down/3, l") + Key("ctrl:up, win:up, alt:up, shift:up"),
+        "dolphin change to left tab":   Key("tab, tab, tab")
+
 
         # My current aenea proxy system isn't allowing to hold down shift or caps lock, so try using Linux AutoKey instead
-        "press caps lock": Key("tab, tab, tab")
+        "press caps lock": Key("ctrl:down/3, win:down/3, alt:down/3, z") + Key("ctrl:up, win:up, alt:up"),
 
         # Konsole shortcuts
         #"search": Key("c-r"),   # Ctrl+R
