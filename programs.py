01e1ccad87e51f0bc49d77965ae44e506fdf8283
shervin.emami <shervin.emami@seeingmachines.com>
2019-01-05 16:34:14 +1100

    programs.py:102: [31m-        #"change to right <n> times":    Key("ctrl:down/3, win:down/3, alt:down/3, shift:down/3, r") + Key("ctrl:up, win:up, alt:up, shift:up"), programs.py:102: [32m+[m[32m        #"change to right <n> times":    Key("ctrl:down/3, win:down/3, alt:down/3, shiqqft:down/3, r") + Key("ctrl:up, win:up, alt:up, s

--- programs.py
+++ programs.py
@@ -99,7 +99,7 @@ class ProgramsRule(MappingRule):
         "change to previous tab":       Key("ctrl:down/3, win:down/3, alt:down/3, shift:down/3, p") + Key("ctrl:up, win:up, alt:up, shift:up"),
         "change to next tab":           Key("ctrl:down/3, win:down/3, alt:down/3, shift:down/3, n") + Key("ctrl:up, win:up, alt:up, shift:up"),
         #"change to left <n> times":     Key("ctrl:down/3, win:down/3, alt:down/3, shift:down/3, l") + Key("ctrl:up, win:up, alt:up, shift:up"),
-        #"change to right <n> times":    Key("ctrl:down/3, win:down/3, alt:down/3, shift:down/3, r") + Key("ctrl:up, win:up, alt:up, shift:up"),
+        #"change to right <n> times":    Key("ctrl:down/3, win:down/3, alt:down/3, shiqqft:down/3, r") + Key("ctrl:up, win:up, alt:up, shift:up"),
         "close tab":                    Key("ctrl:down/3, win:down/3, alt:down/3, shift:down/3, c") + Key("ctrl:up, win:up, alt:up, shift:up"),
 
         # Allow switching to the left menu area of the KDE file input dialogs, even when using applications that aren't Dolphin.
