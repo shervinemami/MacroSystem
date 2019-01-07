cbb6152d7207ec8a9f4c584a982873118c3a7e21
shervin.emami <shervin.emami@seeingmachines.com>
2019-01-07 14:53:12 +1100

    _aenea.py:149: [31m-[m _aenea.py:149: [31m-    # Do not reload anything in these directories or their subdirectories.[m _aenea.py:149: [31m-    dir_reload_blacklist = set(["core"])[m _aenea.py:149: [31m-    macro_dir = "C:\NatLink\NatLink\MacroSystem"[m _aenea.py:149: [31m-[m _aenea.py:149: [31m-    # Unload all grammars if natlinkmain is available.[m _aenea.py:149: [31m-    if natlinkmain:[m _aenea.py:149: [31m-        natlinkmain.unloadEverything()[m _aenea.py:149: [31m-[m _aenea.py:149: [31m-    for name, module in sorted(sys.modules.items()):[m _aenea.py:149: [31m-        if module and hasattr(module, "__file__"):[m _aenea.py:149: [31m-            # Some builtin modules only have a name so module is None or[m _aenea.py:149: [31m-            # do not have a __file__ attribute.  We skip these.[m _aenea.py:149: [31m-            path = module.__file__[m _aenea.py:149: [31m-[m _aenea.py:149: [31m-            # Convert .pyc paths to .py paths.[m _aenea.py:149: [31m-            path = topy(path)[m _aenea.py:149: [31m-[m _aenea.py:149: [31m-            # Do not unimport this module!  This will cause major problems![m _aenea.py:149: [31m-            if (path.startswith(macro_dir) and[m _aenea.py:149: [31m-                not bool(set(path.split(os.path.sep)) & dir_reload_blacklist)[m _aenea.py:149: [31m-                and path != topy(os.path.abspath(__file__))):[m _aenea.py:149: [31m-[m _aenea.py:149: [31m-                print "removing %s from cache" % name[m _aenea.py:149: [31m-[m _aenea.py:149: [31m-                # Remove the module from the cache so that it will be reloaded[m _aenea.py:149: [31m-                # the next time # that it is imported.  The paths for packages[m _aenea.py:149: [31m-                # end with __init__.pyc so this # takes care of them as well.[m _aenea.py:149: [31m-                del sys.modules[name][m _aenea.py:149: [32m+[m[32m    unload_code()[m

--- _aenea.py
+++ _aenea.py
@@ -146,35 +146,7 @@ def reload_code():
 
 def disableKeyboard():
     print "Disabling just the keyboard grammar."
-
-    # Do not reload anything in these directories or their subdirectories.
-    dir_reload_blacklist = set(["core"])
-    macro_dir = "C:\\NatLink\\NatLink\\MacroSystem"
-
-    # Unload all grammars if natlinkmain is available.
-    if natlinkmain:
-        natlinkmain.unloadEverything()
-
-    for name, module in sorted(sys.modules.items()):
-        if module and hasattr(module, "__file__"):
-            # Some builtin modules only have a name so module is None or
-            # do not have a __file__ attribute.  We skip these.
-            path = module.__file__
-
-            # Convert .pyc paths to .py paths.
-            path = topy(path)
-
-            # Do not unimport this module!  This will cause major problems!
-            if (path.startswith(macro_dir) and
-                not bool(set(path.split(os.path.sep)) & dir_reload_blacklist)
-                and path != topy(os.path.abspath(__file__))):
-
-                print "removing %s from cache" % name
-
-                # Remove the module from the cache so that it will be reloaded
-                # the next time # that it is imported.  The paths for packages
-                # end with __init__.pyc so this # takes care of them as well.
-                del sys.modules[name]
+    unload_code()
 
     print "Switching Dragon to Normal mode."
     action = dragonfly.Mimic("switch", "to", "normal", "mode")
