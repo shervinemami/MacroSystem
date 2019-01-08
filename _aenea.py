4ea5e9fc7c4dfbe89592c04fb457a81d94050698
shervin.emami <shervin.emami@seeingmachines.com>
2019-01-08 22:25:43 +1100

    _aenea.py:194: [31m-    aenea.communications.server.greet_user("Shervo")[m _aenea.py:194: [32m+[m[32m    aenea.communications.server.change_OS("Shervo")[m

--- _aenea.py
+++ _aenea.py
@@ -191,7 +191,7 @@ def changeToLinux():
     #"change to Linux":              Key("ctrl:down/3, win:down/3, alt:down/3, l") + Key("ctrl:up, win:up, alt:up"),
     #action = dragonfly.Key("cw-l")
     #action.execute()
-    aenea.communications.server.greet_user("Shervo")
+    aenea.communications.server.change_OS("Shervo")
 
 
 # Switching OSes, when Windows is in a VM on top of a Linux host:
