070bea7bad10ae29616ef4af2602b9addf4be0ff
shervin.emami <shervin.emami@seeingmachines.com>
2019-01-10 22:41:49 +1100

    _aenea.py:179: [31m-    action = dragonfly.Key("win:down/3, l, win:up")[m _aenea.py:179: [32m+[m[32m    action = dragonfly.Key("super:down/3, l, super:up")[m

--- _aenea.py
+++ _aenea.py
@@ -176,7 +176,7 @@ def shervstest():
     print "Running Shervs Test!"
     #"show window list":      Key("win:down/999, tab") + Key("win:up"),
     #"show window list":      Key("w-l") + Key("tab") + Key("down"),
-    action = dragonfly.Key("win:down/3, l, win:up")
+    action = dragonfly.Key("super:down/3, l, super:up")
     action.execute()
     #time.sleep(0.3)
     #action = dragonfly.Key("tab")
