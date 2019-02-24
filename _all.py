eac1baf90ed37fe9bf86f044c0b63fc784004eba
shervin.emami <shervin.emami@seeingmachines.com>
2019-02-24 15:04:12 +1100

    _all.py:16: [31m-#alternatives.append(RuleRef(rule=words.NopeFormatRule()))[m _all.py:16: [32m+[m[32malternatives.append(RuleRef(rule=words.NopeFormatRule()))[m

--- _all.py
+++ _all.py
@@ -13,7 +13,7 @@ alternatives = []
 alternatives.append(RuleRef(rule=keyboard.KeystrokeRule()))
 alternatives.append(RuleRef(rule=words.FormatRule()))
 #alternatives.append(RuleRef(rule=words.ReFormatRule()))
-#alternatives.append(RuleRef(rule=words.NopeFormatRule()))
+alternatives.append(RuleRef(rule=words.NopeFormatRule()))
 alternatives.append(RuleRef(rule=programs.ProgramsRule()))
 root_action = Alternative(alternatives)
 
